9c55010990c851743072b364497f29fb3602a411
ryanden2018 <denlinger.ryan@gmail.com>
2019-07-22 03:49:10 -0500

    make plots look nice

--- src/components/PlotActualizer.js
+++ src/components/PlotActualizer.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import {
-  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip,
+  LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
 const getData = state => {
   const { data } = state.data;
   return data.map( measurement => {
-    return {...measurement};
+    return {
+      ...measurement,
+      at: (new Date(measurement.at))
+        .toTimeString()
+        .match(/^\d\d:\d\d/)[0],
+    };
   });
 };
 
@@ -18,7 +23,7 @@ const PlotActualizer = () => {
 
   if( !data ) return <div></div>;
 
-  //return <div><ul>{data.map(measurement => <li>{measurement.at} : {measurement.value}</li>)}</ul></div>;
+  if( data.length === 0 ) return <div></div>;
 
   return (
     <LineChart
@@ -28,9 +33,10 @@ const PlotActualizer = () => {
     >
       <CartesianGrid strokeDasharray="3 3" />
       <XAxis dataKey="at" />
-      <YAxis type="number" domain={['auto','auto']} />
+      <YAxis unit={data[0].unit} type="number" domain={['auto','auto']} />
       <Tooltip />
-      <Line type="monotone" dot={false} dataKey="value" stroke="#000000" />
+      <Legend />
+      <Line name={data[0].metric} unit={data[0].unit} type="monotone" dot={false} dataKey="value" stroke="#000000" />
     </LineChart>
   );
   
